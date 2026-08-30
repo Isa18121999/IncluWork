be707246749db0bb4b372a2101365b310f5ebace
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-08-30 18:21:43 -0500

    feat: connect candidate profile and CV navigation

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -5,6 +5,8 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import WelcomeScreen from "../screens/WelcomeScreen";
 import CandidateRegister from "../screens/CandidateRegister";
 import CandidateDashboard from "../screens/CandidateDashboard";
+import CandidateProfileScreen from "../screens/CandidateProfileScreen";
+import CVUpload from "../screens/CVUpload";
 
 const Stack = createNativeStackNavigator();
 
@@ -15,6 +17,8 @@ export default function AppNavigator() {
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="CandidateRegister" component={CandidateRegister} />
         <Stack.Screen name="CandidateDashboard" component={CandidateDashboard} />
+        <Stack.Screen name="CandidateProfile" component={CandidateProfileScreen} />
+        <Stack.Screen name="CV" component={CVUpload} />
       </Stack.Navigator>
     </NavigationContainer>
   );
