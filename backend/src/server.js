971a471cc1b7eb8a6d24ae0b4bfb4548df7939c2
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-08-30 18:32:06 -0500

    feat: connect authentication routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const applicationRoutes = require("./routes/applicationRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/applications", applicationRoutes);
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.json({
