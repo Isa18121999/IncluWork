a08c011d787e990492290c15aeb0328e0752e4dd
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-08-30 18:30:14 -0500

    feat: add company model structure

---
+++ backend/src/models/Company.js
@@ -0,0 +1,11 @@
+const Company = {
+  name: "",
+  sector: "",
+  country: "",
+  description: "",
+  verified: false,
+  inclusionPolicy: "",
+  accessibilityOptions: []
+};
+
+module.exports = Company;
