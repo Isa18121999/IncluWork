710d6a6f0e09b8daf29e3cfb95fc387826db743a
Isabella Montoya Medina <97621420+Isa18121999@users.noreply.github.com>
2026-08-31 15:11:00 -0500

    feat: register login screen in navigation

--- mobile/src/navigation/AppNavigator.js
+++ mobile/src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import WelcomeScreen from "../screens/WelcomeScreen";
+import LoginScreen from "../screens/LoginScreen";
 import CandidateRegister from "../screens/CandidateRegister";
 import CandidateDashboard from "../screens/CandidateDashboard";
 import CandidateProfileScreen from "../screens/CandidateProfileScreen";
@@ -20,6 +21,7 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="CandidateRegister" component={CandidateRegister} />
         <Stack.Screen name="CandidateDashboard" component={CandidateDashboard} />
         <Stack.Screen name="CandidateProfile" component={CandidateProfileScreen} />
